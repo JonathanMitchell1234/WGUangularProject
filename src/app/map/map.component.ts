e21b7a9fa36d7d86ed028413395577ddc435598d
Jonathan Mitchell <jonmitchell1234@gmail.com>
2023-08-17 00:18:31 -0400

    added additonal API calls to display more data about the country

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,7 +7,6 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit, AfterViewInit {
-
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
@@ -19,48 +18,65 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
 
   fetchData() {
-    this.http.get('http://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json')
+    this.http.get('http://api.worldbank.org/v2/country/all?format=json')
       .subscribe((data: any) => {
         console.log(data);
       });
   }
 
-  setupSvgEvents() {
+  setupSvgEvents = () => {
     let selectedArea: string | null = null;
     let areas = document.querySelectorAll<SVGElement>('path');
-        areas.forEach((area) => {
-          area.addEventListener('mouseover', () => {
-            area.style.fill = 'green';
-            const country = area.id.toUpperCase();
-            const url = `http://api.worldbank.org/v2/country/${country}/indicator/SP.POP.TOTL?format=json`;
-            this.http.get(url).subscribe((data: any) => {
-              const population = data[1][0].value;
-              const flexItem = document.querySelector('.flex-items:nth-child(2)');
-              if (flexItem) {
-                flexItem.innerHTML = `<h2>${country}</h2><p>Population: ${population}</p>`;
-              }
-            });
-          });
-          area.addEventListener('mouseout', function () {
-            if (selectedArea !== area.id) {
-              area.style.fill = '';
-            }
-          });
-          area.addEventListener('click', function () {
-            console.log(selectedArea);
-            if (selectedArea) {
-              const selectedElement = document.querySelector<SVGElement>(`#${selectedArea}`);
-              if (selectedElement) {
-                selectedElement.setAttribute('class', 'st0');
-              }
-            }
-            if (selectedArea !== area.id) {
-              selectedArea = area.id;
-              area.setAttribute('class', 'selectedArea');
-            } else {
-              selectedArea = null;
-            }
-          });
+    areas.forEach((area) => {
+      area.addEventListener('mouseover', () => {
+        area.style.fill = 'green';
+        const countryCode = area.id.toUpperCase();
+        const url = `http://api.worldbank.org/v2/country/${countryCode}?format=json`;
+        this.http.get(url).subscribe((data: any) => {
+          const countryData = data[1][0];
+          const countryName = countryData.name;
+          const countryCapital = countryData.capitalCity;
+          const countryRegion = countryData.region.value;
+          const incomeLevel = countryData.incomeLevel.value;
+
+          const flexItem = document.querySelector('.flex-items:nth-child(2)');
+          if (flexItem) {
+            flexItem.innerHTML = `
+              <h2>${countryName}</h2>
+              <p>Country Code: ${countryCode}</p>
+              <p>Capital: ${countryCapital}</p>
+              <p>Region: ${countryRegion}</p>
+              <p>Income Level: ${incomeLevel}</p>
+            `;
+          }
         });
-      }
-    }
+      });
+
+      area.addEventListener('mouseout', function () {
+        if (selectedArea !== area.id) {
+          area.style.fill = '';
+        }
+      });
+
+      area.addEventListener('click', function () {
+        if (selectedArea) {
+          const selectedElement = document.querySelector<SVGElement>(`#${selectedArea}`);
+          if (selectedElement) {
+            selectedElement.setAttribute('class', 'st0');
+          }
+        }
+        if (selectedArea !== area.id) {
+          selectedArea = area.id;
+          area.setAttribute('class', 'selectedArea');
+        } else {
+          selectedArea = null;
+        }
+      });
+
+      // Add more event listeners here if needed
+      // area.addEventListener('mousedown', ...);
+      // area.addEventListener('mouseup', ...);
+      // ...
+    });
+  }
+}
