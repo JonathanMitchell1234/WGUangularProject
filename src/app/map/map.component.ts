3e309512083fe4dffbabb5d3b932d04c0afc4626
Jonathan Mitchell <jonmitchell1234@gmail.com>
2023-08-18 18:04:48 -0400

    added faded in animations and made minor CSS tweaks

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -16,16 +16,18 @@ export class MapComponent implements AfterViewInit {
   setupSvgEvents = () => {
     let selectedArea: string | null = null;
     let areas = document.querySelectorAll<SVGElement>('path');
+    // forEach loop adds event listeners to each path element
     areas.forEach((area) => {
       // event listener calls the API using Angular's HTTPclient when the mouse enters, country is highlighted green
       area.addEventListener('mouseover', () => {
         area.style.fill = 'green';
         const countryCode = area.id.toUpperCase();
+        // if the country has no isocode2, the API call is not made
         if (area.id === '') {
           console.log('no isocode2 for this path');
           return;
         }
-        // API is called using a template literal that uses the country's isocode2 to dynamically update on mouse enter
+        // API is called using a template literal that uses the country's isocode2 ID to dynamically update on mouse enter
         const url = `http://api.worldbank.org/v2/country/${countryCode}?format=json`;
         this.http.get(url).subscribe((data: any) => {
           const countryData = data[1][0];
@@ -35,10 +37,9 @@ export class MapComponent implements AfterViewInit {
           const incomeLevel = countryData.incomeLevel.value;
           const longitude = countryData.longitude;
           const latitude = countryData.latitude;
-
           const flexItem = document.querySelector('.flex-items:nth-child(2)');
           if (flexItem) {
-            // data is parsed into the empty div
+            // data and HTML is injected into an empty div on the right column of map
             flexItem.innerHTML = `
               <h2>${countryName}</h2>
               <p>Country Code: ${countryCode}</p>
@@ -48,6 +49,12 @@ export class MapComponent implements AfterViewInit {
               <p>Longitude: ${longitude}</p>
               <p>Latitude: ${latitude}</p>
             `;
+            // Remove the 'fade-in' class first to trigger the animation on subsequent mouseovers
+            flexItem.classList.remove('fade-in');
+            // Adding the class with a slight delay to re-trigger the animation
+            setTimeout(() => {
+              flexItem.classList.add('fade-in');
+            }, 10);
           }
         });
       });
